047ca8960cea4b35cd53c73136cf6b42897019e4
Sheng Huang <103220922+shengh318@users.noreply.github.com>
2026-06-02 08:05:25 -0400

    new sprites

--- Characters/Stickman.gd
+++ Characters/Stickman.gd
@@ -6,8 +6,9 @@ const GRAVITY = 2800.0
 const MAX_JUMPS = 2
 
 const FRAME_W = 64
-const SPRITE_DIR = "res://Sprites/LPC/split/standard/"
+var sprite_dir := "res://Sprites/LPC/split/standard/"
 
+@export var is_player := true
 var facing_right := true
 var jump_was_pressed := false
 var jump_count := 0
@@ -16,10 +17,15 @@ var last_down := false
 var drop_until := 0.0
 
 var action_locked := false
+var respawn_lock := false
+var respawn_landed := false
 var last_melee := false
 var last_ranged := false
 var last_cast := false
 var last_dodge := false
+
+var weapon: Node2D
+var weapon_pivot: Node2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -38,15 +44,21 @@ const ANIM_DATA = {
 }
 
 func _ready() -> void:
+	if not is_player:
+		sprite_dir = "res://Sprites/LPC/split_p2/standard/"
+		facing_right = false
 	_build_sprite_frames()
 	anim.play("idle")
 	platform_mask = 1 | 2
+	if not is_player:
+		anim.flip_h = false
+	_create_weapon()
 
 func _build_sprite_frames() -> void:
 	var frames = SpriteFrames.new()
 	for name in ANIM_DATA:
 		var d = ANIM_DATA[name]
-		var tex = load(SPRITE_DIR + d.file)
+		var tex = load(sprite_dir + d.file)
 		if not tex:
 			continue
 		frames.add_animation(name)
@@ -64,35 +76,38 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 
-	var left = Input.is_key_pressed(KEY_A)
-	var right = Input.is_key_pressed(KEY_D)
-
-	var h_dir := 0.0
-	if left:
-		h_dir -= 1.0
-	if right:
-		h_dir += 1.0
-
-	var jump_pressed := Input.is_key_pressed(KEY_SPACE)
-	if jump_pressed and not jump_was_pressed and jump_count < MAX_JUMPS:
-		velocity.y = JUMP_VELOCITY
-		jump_count += 1
-	jump_was_pressed = jump_pressed
 	if is_on_floor():
 		jump_count = 0
 
-	if h_dir != 0:
-		velocity.x = h_dir * SPEED
-		facing_right = h_dir > 0
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var h_dir := 0.0
 
-	var down_pressed := Input.is_key_pressed(KEY_S)
-	var down_just_pressed := down_pressed and not last_down
-	last_down = down_pressed
+	if is_player and not respawn_lock:
+		var left = Input.is_key_pressed(KEY_A)
+		var right = Input.is_key_pressed(KEY_D)
 
-	if down_just_pressed and is_on_floor():
-		drop_until = Time.get_ticks_msec() + 300.0
+		if left:
+			h_dir -= 1.0
+		if right:
+			h_dir += 1.0
+
+		var jump_pressed := Input.is_key_pressed(KEY_SPACE)
+		if jump_pressed and not jump_was_pressed and jump_count < MAX_JUMPS:
+			velocity.y = JUMP_VELOCITY
+			jump_count += 1
+		jump_was_pressed = jump_pressed
+
+		if h_dir != 0:
+			velocity.x = h_dir * SPEED
+			facing_right = h_dir > 0
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
+		var down_pressed := Input.is_key_pressed(KEY_S)
+		var down_just_pressed := down_pressed and not last_down
+		last_down = down_pressed
+
+		if down_just_pressed and is_on_floor():
+			drop_until = Time.get_ticks_msec() + 300.0
 
 	var should_drop := Time.get_ticks_msec() < drop_until
 
@@ -103,7 +118,15 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+	if respawn_lock and is_on_floor() and not respawn_landed:
+		respawn_landed = true
+		get_tree().create_timer(1.0).timeout.connect(func():
+			respawn_lock = false
+		)
+
 	anim.flip_h = facing_right
+	if weapon:
+		weapon.scale.x = 1 if facing_right else -1
 
 	if action_locked:
 		if not anim.is_playing():
@@ -111,31 +134,35 @@ func _physics_process(delta: float) -> void:
 		else:
 			return
 
-	var melee_now = Input.is_key_pressed(KEY_J)
-	var ranged_now = Input.is_key_pressed(KEY_K)
-	var cast_now = Input.is_key_pressed(KEY_L)
-	var dodge_now = Input.is_key_pressed(KEY_H)
+	if is_player:
+		var melee_now = Input.is_key_pressed(KEY_P)
+		var ranged_now = Input.is_key_pressed(KEY_K)
+		var cast_now = Input.is_key_pressed(KEY_L)
+		var dodge_now = Input.is_key_pressed(KEY_H)
 
-	var melee = melee_now and not last_melee
-	var ranged = ranged_now and not last_ranged
-	var cast = cast_now and not last_cast
-	var dodge = dodge_now and not last_dodge
+		var melee = melee_now and not last_melee
+		var ranged = ranged_now and not last_ranged
+		var cast = cast_now and not last_cast
+		var dodge = dodge_now and not last_dodge
 
-	last_melee = melee_now
-	last_ranged = ranged_now
-	last_cast = cast_now
-	last_dodge = dodge_now
+		last_melee = melee_now
+		last_ranged = ranged_now
+		last_cast = cast_now
+		last_dodge = dodge_now
 
-	if dodge:
-		_start_action("dodge")
-	elif melee:
-		_start_action("attack")
-	elif ranged:
-		_start_action("ranged")
-	elif cast:
-		_start_action("spell")
-	elif anim.animation == "death":
+		if not respawn_lock and dodge:
+			_start_action("dodge")
+		elif not respawn_lock and melee:
+			_start_action("attack")
+		elif not respawn_lock and ranged:
+			_start_action("ranged")
+		elif not respawn_lock and cast:
+			_start_action("spell")
+
+	if anim.animation == "death":
 		pass
+	elif respawn_lock:
+		_play_anim("idle")
 	elif not is_on_floor():
 		_play_anim("jump")
 	elif h_dir != 0:
@@ -144,10 +171,51 @@ func _physics_process(delta: float) -> void:
 		_play_anim("idle")
 
 
+func _create_weapon() -> void:
+	weapon = Node2D.new()
+	weapon.name = "Weapon"
+	weapon.position = Vector2(10, -6)
+	add_child(weapon)
+
+	weapon_pivot = Node2D.new()
+	weapon_pivot.name = "Pivot"
+	weapon_pivot.rotation = -0.4
+	weapon.add_child(weapon_pivot)
+
+	var blade = ColorRect.new()
+	blade.name = "Blade"
+	blade.size = Vector2(4, 34)
+	blade.color = Color(0.75, 0.75, 0.85)
+	blade.position = Vector2(-2, -34)
+	weapon_pivot.add_child(blade)
+
+	var guard = ColorRect.new()
+	guard.name = "Guard"
+	guard.size = Vector2(10, 2)
+	guard.color = Color(0.4, 0.3, 0.2)
+	guard.position = Vector2(-5, -2)
+	weapon_pivot.add_child(guard)
+
+	var handle = ColorRect.new()
+	handle.name = "Handle"
+	handle.size = Vector2(3, 7)
+	handle.color = Color(0.3, 0.2, 0.1)
+	handle.position = Vector2(-1, 0)
+	weapon_pivot.add_child(handle)
+
+func _swing_weapon() -> void:
+	if not weapon_pivot:
+		return
+	weapon_pivot.rotation = -1.8
+	var tween = create_tween()
+	tween.tween_property(weapon_pivot, "rotation", 1.5, 0.12).set_ease(Tween.EASE_OUT)
+
 func _start_action(name: String) -> void:
 	action_locked = true
 	anim.play(name)
 	anim.advance(0)
+	if name == "attack":
+		_swing_weapon()
 
 func _play_anim(name: String) -> void:
 	if anim.animation != name:
@@ -164,3 +232,24 @@ func die() -> void:
 	action_locked = true
 	anim.play("death")
 	anim.advance(0)
+
+func respawn() -> void:
+	velocity = Vector2.ZERO
+	jump_count = 0
+	facing_right = true
+	action_locked = false
+	respawn_lock = true
+	respawn_landed = false
+	drop_until = 0.0
+	last_down = false
+	jump_was_pressed = false
+	last_melee = false
+	last_ranged = false
+	last_cast = false
+	last_dodge = false
+	anim.play("idle")
+	anim.flip_h = facing_right
+	anim.frame = 0
+	if weapon:
+		weapon_pivot.rotation = -0.4
+		weapon.scale.x = 1 if facing_right else -1
--- Scenes/Arena.gd
+++ Scenes/Arena.gd
@@ -1,41 +1,49 @@
 extends Node2D
 
+var stickman: CharacterBody2D
+
+var respawn_node: Node2D = null
+var respawn_life: float = 0.0
+var is_respawning: bool = false
+
+const FALL_DEATH_Y = 1120
+const RESPAWN_DURATION = 3.0
+const PLATFORM_FALL_DURATION = 0.8
+const PLATFORM_X = 960
+const PLATFORM_Y_TARGET = 320.0
+const PLATFORM_Y_START = -250.0
+const PLATFORM_W = 280
+const PLATFORM_H = 24
+const PLATFORM_COLOR = Color(0.5, 0.7, 0.9, 0.35)
+const FLASH_DURATION = 0.8
+
+var platform_rects: Array[Rect2] = []
+
 @onready var pause_menu: CanvasLayer = $PauseMenu
 
-var platform_data := []
-
 func _ready() -> void:
+	stickman = $Stickman
 	pause_menu.hide()
 	$PauseMenu/ResumeButton.pressed.connect(_on_resume_pressed)
 	$PauseMenu/QuitButton.pressed.connect(_on_quit_pressed)
 
 	for child in get_children():
 		if child.name.begins_with("Platform"):
-			var shape = child.get_node("CollisionShape2D").shape
-			var r = Rect2(
-				child.position.x - shape.size.x / 2,
-				child.position.y - shape.size.y / 2,
-				shape.size.x,
-				shape.size.y
-			)
-			platform_data.append({"rect": r, "color": Color(0.35, 0.35, 0.35)})
-		elif child.name == "KillFloor":
-			var shape = child.get_node("CollisionShape2D").shape
-			var r = Rect2(
-				child.position.x - shape.size.x / 2,
-				child.position.y - shape.size.y / 2,
-				shape.size.x,
-				shape.size.y
-			)
-			platform_data.append({"rect": r, "color": Color(0.6, 0.1, 0.1, 0.5)})
-
+			var shape = child.get_node("CollisionShape2D").shape as RectangleShape2D
+			if shape:
+				platform_rects.append(Rect2(
+					child.position.x - shape.size.x / 2,
+					child.position.y - shape.size.y / 2,
+					shape.size.x,
+					shape.size.y
+				))
 	queue_redraw()
 
 func _draw() -> void:
-	for p in platform_data:
-		draw_rect(p.rect, p.color)
+	for r in platform_rects:
+		draw_rect(r, Color(0.35, 0.35, 0.35))
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		if get_tree().paused:
 			pause_menu.hide()
@@ -43,6 +51,69 @@ func _process(_delta: float) -> void:
 		else:
 			pause_menu.show()
 			get_tree().paused = true
+
+	if is_respawning:
+		update_respawn_platform(delta)
+	elif stickman and stickman.global_position.y > FALL_DEATH_Y:
+		start_respawn()
+
+func start_respawn() -> void:
+	is_respawning = true
+	stickman.hide()
+	stickman.set_physics_process(false)
+	stickman.set_process(true)
+
+	create_respawn_platform()
+
+func create_respawn_platform() -> void:
+	respawn_node = Node2D.new()
+	add_child(respawn_node)
+	move_child(respawn_node, stickman.get_index())
+	respawn_node.position = Vector2(PLATFORM_X, PLATFORM_Y_START)
+
+	var rect = ColorRect.new()
+	rect.name = "Visual"
+	rect.color = PLATFORM_COLOR
+	rect.size = Vector2(PLATFORM_W, PLATFORM_H)
+	rect.position = Vector2(-PLATFORM_W / 2, -PLATFORM_H / 2)
+	respawn_node.add_child(rect)
+
+	var body = StaticBody2D.new()
+	body.name = "Body"
+	body.collision_layer = 2
+	var shape = CollisionShape2D.new()
+	shape.shape = RectangleShape2D.new()
+	shape.shape.size = Vector2(PLATFORM_W, PLATFORM_H)
+	body.add_child(shape)
+	respawn_node.add_child(body)
+
+	var tween = create_tween()
+	tween.tween_property(respawn_node, "position:y", PLATFORM_Y_TARGET, PLATFORM_FALL_DURATION).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
+	tween.tween_callback(_on_platform_landed)
+
+func _on_platform_landed() -> void:
+	respawn_life = RESPAWN_DURATION
+
+	var platform_top = respawn_node.position.y - PLATFORM_H / 2
+	stickman.global_position = Vector2(respawn_node.position.x, platform_top - 20)
+	stickman.respawn()
+	stickman.show()
+	stickman.set_physics_process(true)
+
+func update_respawn_platform(delta: float) -> void:
+	respawn_life -= delta
+	if respawn_life <= 0:
+		if respawn_node:
+			respawn_node.queue_free()
+			respawn_node = null
+		is_respawning = false
+		return
+
+	if respawn_life < FLASH_DURATION and respawn_node:
+		var rect = respawn_node.get_node_or_null("Visual") as ColorRect
+		if rect:
+			var flash_hz = 5.0 + (FLASH_DURATION - respawn_life) * 15.0
+			rect.visible = int(Time.get_ticks_msec() * flash_hz / 1000.0) % 2 == 0
 
 func _on_resume_pressed() -> void:
 	pause_menu.hide()
